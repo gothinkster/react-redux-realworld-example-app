c7fa7e9c6fe43104b5cbfd3916de59013cdf1354
Eric Simons <ericsimons@Erics-MacBook.local>
2016-11-02 11:17:10 -0700

    step 01

--- src/index.js
+++ src/index.js
@@ -1,14 +1,47 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import { createStore } from 'redux';
+
+const defaultState = { checked: false };
+const reducer = function(state = defaultState, action) {
+  switch (action.type) {
+    case 'TOGGLE':
+      return { ...state, checked: !state.checked };
+  }
+  return state;
+};
+const store = createStore(reducer);
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  componentWillMount() {
+    store.subscribe(() => this.setState(store.getState()));
+  }
+
   render() {
+    const onClick = () => store.dispatch({ type: 'TOGGLE' });
     return (
-      <h1>Hello, World!</h1>
+      <div>
+        <h1>To-dos</h1>
+        <div>
+          Learn Redux&nbsp;
+          <input
+            type="checkbox"
+            checked={!!this.state.checked}
+            onClick={onClick} />
+        </div>
+        {
+          this.state.checked ? (<h2>Done!</h2>) : null
+        }
+      </div>
     );
   }
 }
 
 ReactDOM.render((
   <App />
-), document.getElementById('root'));
+), document.getElementById('r'));
