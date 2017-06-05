afe1b9a43745c583767bae54ada94e809502a61a
Justin Joyce <justinwjoyce@gmail.com>
2017-06-05 15:34:09 -0500

    Add register function in src/reducers/agent.js

--- src/agent.js
+++ src/agent.js
@@ -33,6 +33,8 @@ const Auth = {
     requests.get('/user'),
   login: (email, password) =>
     requests.post('/users/login', { user: { email, password } })
+  register: (username, email, password) =>
+    requests.post('/users', { user: { username, email, password } })
 };
 
 export default {
