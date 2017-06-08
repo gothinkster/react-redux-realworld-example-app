4d2dfb996c49fa418c18c92744362eefa645503a
Justin Joyce <justinwjoyce@gmail.com>
2017-06-08 15:30:55 -0500

    finsih up branch 05

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,7 +12,9 @@ const mapDispatchToProps = dispatch => ({
   onChangePassword: value =>
     dispatch({ type: 'UPDATE_FIELD_AUTH', key: 'password', value }),
   onSubmit: (email, password) =>
-    dispatch({ type: 'LOGIN', payload: agent.Auth.login(email, password) })
+    dispatch({ type: 'LOGIN', payload: agent.Auth.login(email, password) }),
+  onUnload: () =>
+    dispatch({ type: 'LOGIN_PAGE_UNLOADED' })
 });
 
 class Login extends React.Component {
@@ -24,6 +26,9 @@ class Login extends React.Component {
       ev.preventDefault();
       this.props.onSubmit(email, password);
     };
+   componentWillUnmount() {
+    this.props.onUnload();
+    }
   }
 
   render() {
--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -7,6 +7,9 @@ export default (state = {}, action) => {
         inProgress: false,
         errors: action.error ? action.payload.errors : null
       };
+    case 'LOGIN_PAGE_UNLOADED':
+    case 'REGISTER_PAGE_UNLOADED':
+      return {};
     case 'ASYNC_START':
       if (action.subtype === 'LOGIN' || action.subtype === 'REGISTER') {
         return { ...state, inProgress: true };
