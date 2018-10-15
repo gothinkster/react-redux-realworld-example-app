ee56d3bb315ded544f5a13b49a098af6bbdeb117
Charlie Wyman <charlie.wyman@ruralsource.com>
2018-10-15 09:12:56 -0400

    corrected package installation locally

--- src/__tests__/components/example.test.js
+++ src/__tests__/components/example.test.js
@@ -4,21 +4,11 @@ import { shallow } from 'enzyme';
 import configureStore from "redux-mock-store";
 
 describe("Renders Register component", () => {
-    const state = {
-        auth: {
-
-        }
-    };
     const mockStore = configureStore();    
     let wrapper;
-    beforeEach(() => {
-       
-    });  
-
     
     it('renders the form', () => {
-        wrapper = shallow(<Register />);         
-        console.log(wrapper);
+        const wrapper = shallow(<Register />);         
         expect(wrapper.find('form').exists()).toBe(true);
     });  
 });
