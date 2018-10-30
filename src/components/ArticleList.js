2cf0c01fcd348a50396368f17a4314a9b3b093b3
Charlie Wyman <charlie.wyman@ruralsource.com>
2018-10-30 15:59:41 -0400

    resolved issue with crash on no server connection

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -3,6 +3,7 @@ import ArticlePreview from "./ArticlePreview";
 import ListPagination from "./ListPagination";
 
 const ArticleList = props => {
+  let pageArticles;
   const { articles, pager, currentPage } = props;
   if (!articles) {
     return <div className="article-preview">Loading...</div>;
@@ -10,9 +11,9 @@ const ArticleList = props => {
 
   if (!articles.length) {
     return <div className="article-preview">No articles are here... yet.</div>;
+  } else {
+    pageArticles = articles.slice(currentPage, currentPage + 5);
   }
-  console.log(articles.length);
-  const pageArticles = articles.slice(currentPage, currentPage + 5);
 
   return (
     <React.Fragment>
