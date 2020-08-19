695c40778044cf799d257cdb5d76edd72a913825
McEwan <romcewan@f8ffc202c4fc.ant.amazon.com>
2020-08-19 16:47:55 +0200

    point api at oktank

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,7 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = 'https://conduit.productionready.io/api';
+const API_ROOT = 'https://oktank-backend.herokuapp.com/api';
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
