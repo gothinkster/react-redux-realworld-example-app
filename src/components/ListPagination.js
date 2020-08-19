b9107da9d26dd7ca18e0e321151050784bb92960
Praveen Kumar <praveengopi16@gmail.com>
2020-08-19 13:24:37 +0530

    refactor!: Pagination with First/Last Previous/Next

--- src/components/ListPagination.js
+++ src/components/ListPagination.js
@@ -13,43 +13,103 @@ const ListPagination = props => {
     return null;
   }
 
+  const PageCount = Math.ceil(props.articlesCount / 10);
+
+  const currentPage = props.currentPage + 1; //since api has current page  from 0 to Pagecount
   const range = [];
-  for (let i = 0; i < Math.ceil(props.articlesCount / 10); ++i) {
-    range.push(i);
+
+  let index; //variable for pushing Page number in an array
+
+  let PageListCount = 5; //we can modify according to the pagination list count we needed
+
+  if (PageCount < PageListCount) {
+    //incase we have number of pages less than the page list i.e., 3 pages at 5 pagination list
+    PageListCount = PageCount;
   }
 
+  if (currentPage <= Math.floor(PageListCount / 2)) {
+    index = 1;
+  }
+  else if (currentPage >= PageCount - Math.floor(PageListCount / 2)) {
+    index = PageListCount % 2 === 0 ? PageCount - Math.floor(PageListCount / 2) * 2 + 1 : PageCount - Math.floor(PageListCount / 2) * 2;
+    //to maintain the current page as center and to avoid the overflow of page number beyond the pagecount
+  }
+  else {
+    index = currentPage - Math.floor(PageListCount / 2); //to maintain the current page as center 
+  }
+
+  for (let i = 1; i <= PageListCount; ++index, i++) {
+    range.push(index);
+  }
+
+
   const setPage = page => {
-    if(props.pager) {
+    if (props.pager) {
       props.onSetPage(page, props.pager(page));
-    }else {
+    } else {
       props.onSetPage(page, agent.Articles.all(page))
     }
+  };
+
+  const onClick = (ev, v) => {
+    ev.preventDefault();
+
+    if (props.currentPage === v - 1) {
+      //if same page we stop processing the next request
+      return null;
+    }
+
+    if (v > 0 && v <= PageCount) {
+      setPage(v - 1);
+    }
+
   };
 
   return (
     <nav>
       <ul className="pagination">
 
+        <li
+          className={'page-item'}
+          onClick={(ev) => onClick(ev, 1)}
+          key={"first"}>
+          <a className="page-link" href="">First</a>
+        </li>
+
+        <li
+          className={'page-item'}
+          onClick={(ev) => onClick(ev, currentPage - 1)}
+          key={"previous"}>
+          <a className="page-link" href="">Previous</a>
+        </li>
+
         {
+
           range.map(v => {
-            const isCurrent = v === props.currentPage;
-            const onClick = ev => {
-              ev.preventDefault();
-              setPage(v);
-            };
+            const isCurrent = v === currentPage;
             return (
               <li
-                className={ isCurrent ? 'page-item active' : 'page-item' }
-                onClick={onClick}
+                className={isCurrent ? 'page-item active' : 'page-item'}
+                onClick={(ev) => onClick(ev, v)}
                 key={v.toString()}>
-
-                <a className="page-link" href="">{v + 1}</a>
-
+                <a className="page-link" href="">{v}</a>
               </li>
             );
           })
         }
 
+        <li
+          className={'page-item'}
+          onClick={(ev) => onClick(ev, currentPage + 1)}
+          key={"next"}>
+          <a className="page-link" href="">Next</a>
+        </li>
+        <li
+          className={'page-item'}
+          onClick={(ev) => onClick(ev, PageCount)}
+          key={"last"}>
+          <a className="page-link" href="">Last</a>
+        </li>
       </ul>
     </nav>
   );
