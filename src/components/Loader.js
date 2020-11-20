d8b83ecf5e036556ad6dcae30b02ebd74dd18408
Phillip Musiime <phillipmusiime@gmail.com>
2020-11-20 09:29:22 +0300

    import emotion and style spinner

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { css } from "@emotion/core";
 import { PropagateLoader } from 'react-spinners';
 
 const Loader = (WrappedComponent) => {
@@ -10,13 +11,22 @@ const Loader = (WrappedComponent) => {
     };
     return (
       <div>
-        {isLoading && <PropagateLoader loading={isLoading} />}
+        {isLoading && <PropagateLoader css={override} color={"#62B85C"} loading={isLoading} />}
         <WrappedComponent {...props} setLoading={setLoadingState} />
       </div>
     )
   }
   return HOC;
 }
+
+const override = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+`;
 
 export default Loader;
 
