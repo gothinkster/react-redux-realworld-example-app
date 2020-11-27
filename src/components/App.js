52719e1cae6546b7f7984015a8aff515f428f621
paresh <paresh_meb@yahoo.com>
2020-11-28 09:25:32 +1000

    updating Editor

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import agent from '../agent';
 import Header from './Header';
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
-import { APP_LOAD, REDIRECT } from '../constants/actionTypes';
-import { Route, Switch } from 'react-router-dom';
+import React, {useEffect} from 'react';
+import {connect} from 'react-redux';
+import {APP_LOAD, REDIRECT} from '../constants/actionTypes';
+import {Route, Switch} from 'react-router-dom';
 import Article from '../components/Article';
 import Editor from '../components/Editor';
 import Home from '../components/Home';
@@ -12,66 +12,66 @@ import Profile from '../components/Profile';
 import ProfileFavorites from '../components/ProfileFavorites';
 import Register from '../components/Register';
 import Settings from '../components/Settings';
-import { store } from '../store';
-import { push } from 'react-router-redux';
+import {store} from '../store';
+import {push} from 'react-router-redux';
 
 const mapStateToProps = state => {
-  return {
-    appLoaded: state.common.appLoaded,
-    appName: state.common.appName,
-    currentUser: state.common.currentUser,
-    redirectTo: state.common.redirectTo
-  }};
+    return {
+        appLoaded: state.common.appLoaded,
+        appName: state.common.appName,
+        currentUser: state.common.currentUser,
+        redirectTo: state.common.redirectTo
+    }
+};
 
 const mapDispatchToProps = dispatch => ({
-  onLoad: (payload, token) =>
-    dispatch({ type: APP_LOAD, payload, token, skipTracking: true }),
-  onRedirect: () =>
-    dispatch({ type: REDIRECT })
+    onLoad: (payload, token) =>
+        dispatch({type: APP_LOAD, payload, token, skipTracking: true}),
+    onRedirect: () =>
+        dispatch({type: REDIRECT})
 });
 
-function App(props) {
-  useEffect(() => {
-    if (props.redirectTo) {
-      // this.context.router.replace(nextProps.redirectTo);
-      store.dispatch(push(props.redirectTo));
-      props.onRedirect();
-    }
-    // settings token
-    const token = window.localStorage.getItem('jwt');
-    if (token) {
-      agent.setToken(token);
-    }
-    props.onLoad(token ? agent.Auth.current() : null, token);
-  }, []);
+const App = (props) => {
+    useEffect(() => {
+        if (props.redirectTo) {
+            store.dispatch(push(props.redirectTo));
+            props.onRedirect();
+        }
+        // settings token
+        const token = window.localStorage.getItem('jwt');
+        if (token) {
+            agent.setToken(token);
+        }
+        props.onLoad(token ? agent.Auth.current() : null, token);
+    }, []);
 
     if (props.appLoaded) {
-      return (
-        <div>
-          <Header
-            appName={props.appName}
-            currentUser={props.currentUser} />
-            <Switch>
-            <Route exact path="/" component={Home}/>
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-            <Route path="/editor/:slug" component={Editor} />
-            <Route path="/editor" component={Editor} />
-            <Route path="/article/:id" component={Article} />
-            <Route path="/settings" component={Settings} />
-            <Route path="/@:username/favorites" component={ProfileFavorites} />
-            <Route path="/@:username" component={Profile} />
-            </Switch>
-        </div>
-      );
+        return (
+            <div>
+                <Header
+                    appName={props.appName}
+                    currentUser={props.currentUser}/>
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/login" component={Login}/>
+                    <Route path="/register" component={Register}/>
+                    <Route path="/editor/:slug" component={Editor}/>
+                    <Route path="/editor" component={Editor}/>
+                    <Route path="/article/:id" component={Article}/>
+                    <Route path="/settings" component={Settings}/>
+                    <Route path="/@:username/favorites" component={ProfileFavorites}/>
+                    <Route path="/@:username" component={Profile}/>
+                </Switch>
+            </div>
+        );
     } else {
-      return (
-          <div>
-            <Header
-                appName={props.appName}
-                currentUser={props.currentUser} />
-          </div>
-      );
+        return (
+            <div>
+                <Header
+                    appName={props.appName}
+                    currentUser={props.currentUser}/>
+            </div>
+        );
     }
 }
 
