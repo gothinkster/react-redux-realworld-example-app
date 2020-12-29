13e6b57b473d11ab2b1e7f68c69f926a78814b24
Tamas Sallai <gsashee@gmail.com>
2020-12-29 12:17:32 +0100

    add backend URL environment parameter

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,7 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = 'https://conduit.productionready.io/api';
+const API_ROOT = process.env.REACT_APP_BACKEND_URL || 'https://conduit.productionready.io/api';
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
