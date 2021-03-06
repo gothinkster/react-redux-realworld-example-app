68b9cc67baa796e79912b0d5954c473c1ab1c855
gunshykennedy <lakisbenjamin@gmail.com>
2021-03-06 15:22:00 -0600

    created App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,13 @@
 import React from "react";
+import { connect } from "react-redux";
 
+const mapStateToProps = (state) => ({
+  appName: state.appName,
+});
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
-
-  componentWillMount() {
-    store.subscribe(() => this.setState(store.getState()));
-  }
-
-  componentWillMount() {
-    store.subscribe(() => this.setState(store.getState()));
-  }
-
   render() {
-    const onClick = () => store.dispatch({ type: "TOGGLE" });
-    return (
-      <div>
-        <h1>Hey, Y'all!</h1>
-        <div>
-          Learn Redux
-          <input
-            type="checkbox"
-            checked={!!this.state.checked}
-            onClick={onClick}
-          />
-        </div>
-        {this.state.checked ? <h2>Done!</h2> : null}
-      </div>
-    );
+    return <div>{this.props.appName}</div>;
   }
 }
+
+export default connect(mapStateToProps, () => ({}))(App);
