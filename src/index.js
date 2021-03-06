d99201905878018681f1ebad85165bc6413d630c
gunshykennedy <lakisbenjamin@gmail.com>
2021-03-06 12:56:29 -0600

    render function updated w/ checkbox

--- src/index.js
+++ src/index.js
@@ -13,11 +13,35 @@ return state;
 const store = createStore(reducer);
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  componentWillMount() {
+    store.subscribe(() => this.setState(store.getState()));
+  }
+
+
   render() {
     return (
-      <h1>Hey, Y'all!</h1>
+      <div>
+        <h1>Shit-To-Do</h1>
+        <div>
+          Learn Redux&nbsp;
+          <input
+            type="checkbox"
+            checked={!!this.state.checked}
+          />
+        </div>
+        {
+          this.state.checked ? (<h2>Done!</h2>) : null
+        }
+      </div>
     );
   }
+
+  
 }
 
 ReactDOM.render((
