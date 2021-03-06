894132710dcc0e98434d2653b4f86bfd34ad3803
gunshykennedy <lakisbenjamin@gmail.com>
2021-03-06 12:51:52 -0600

    Crackified Intro and 1st reducer func

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import { createStore } from 'redux';
+
+const deaultState = { checked: false };
+const reducer = function(state = defaultState, action) {
+  switch (action,type) {
+    case 'TOGGLE';
+      return { ...state, checked: !state.checked };
+  }
+return state;
+};
+const store = createStore(reducer);
 
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello, World!</h1>
+      <h1>Hey, Y'all!</h1>
     );
   }
 }
