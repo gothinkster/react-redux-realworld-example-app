5bd0d0ed7d9d8af6b70a55bee09edde68fb3d7bf
gunshykennedy <lakisbenjamin@gmail.com>
2021-03-06 13:25:06 -0600

    Refactored Code

--- src/index.js
+++ src/index.js
@@ -1,44 +1,22 @@
+import App from "./components/App";
+import { Provider } from "react-redux";
 import ReactDOM from "react-dom";
 import React from "react";
 import { createStore } from "redux";
 
-const deaultState = { checked: false };
+const deaultState = {
+  appName: "Boof-xyz",
+  articles: null,
+};
 const reducer = function (state = defaultState, action) {
-  switch ((action, type)) {
-    case "TOGGLE":
-      return { ...state, checked: !state.checked };
-  }
   return state;
 };
+
 const store = createStore(reducer);
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
-
-  componentWillMount() {
-    store.subscribe(() => this.setState(store.getState()));
-  }
-
-  render() {
-    const onClick = () => store.dispatch({ type: "TOGGLE" });
-    return (
-      <div>
-        <h1>Shit-To-Do</h1>
-        <div>
-          Learn Redux&nbsp;
-          <input
-            type="checkbox"
-            checked={!!this.state.checked}
-            onClick={onClick}
-          />
-        </div>
-        {this.state.checked ? <h2>Done!</h2> : null}
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
