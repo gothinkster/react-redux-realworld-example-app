92addffdb7149e5664f28c0b989c032e6852ebe3
gunshykennedy <lakisbenjamin@gmail.com>
2021-03-07 18:36:03 -0600

    moved store from index.js to seperate file

--- src/index.js
+++ src/index.js
@@ -2,22 +2,7 @@ import App from "./components/App";
 import { Provider } from "react-redux";
 import ReactDOM from "react-dom";
 import React from "react";
-import { createStore } from "redux";
-import { promiseMiddleware } from "./middleware";
-
-const deaultState = {
-  appName: "Boof-xyz",
-  articles: null,
-};
-const reducer = function (state = defaultState, action) {
-  switch (action.type) {
-    case "HOME_PAGE_LOADED":
-      return { ...state, articles: action.payload.articles };
-  }
-  return state;
-};
-
-const store = createStore(reducer, applyMiddleware(promiseMiddleware));
+import store from "./store";
 
 ReactDOM.render(
   <Provider store={store}>
