b3f7e814de5ee179093d4b9466e9454be81870a7
gunshykennedy <lakisbenjamin@gmail.com>
2021-03-07 18:46:35 -0600

    Login.js made

--- src/components/App.js
+++ src/components/App.js
@@ -11,10 +11,14 @@ class App extends React.Component {
     return (
       <div>
         <Header appName={this.props.appName} />
-        <Home />
+        {this.props.children}
       </div>
     );
   }
 }
+
+App.contextTypes = {
+  router: React.PropTypes.object.isRequired,
+};
 
 export default connect(mapStateToProps, () => ({}))(App);
--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import { Provider } from "react-redux";
 import ReactDOM from "react-dom";
 import React from "react";
 import store from "./store";
+import Home from "./components/Home";
+import Login from "./components/Login";
+
+import { Router, Route, IndexRoute, hashHistory } from "react-router";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router history={hashHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Home} />
+        <Route path="login" component={Login} />
+      </Route>
+    </Router>
   </Provider>,
   document.getElementById("root")
 );
