880fa84eea4ccfe310f4aaac397b6fe20db7c648
Anuj <49936476+anujrajkumar@users.noreply.github.com>
2021-04-20 11:49:03 +0530

    Update agent.js

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,7 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = 'https://conduit.productionready.io/api';
+const API_ROOT = 'http://localhost:8888/api';
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
