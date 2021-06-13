6157cf91f5fff984e1a6c567f470983c74fdf3ae
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2021-06-12 23:58:36 -0600

    Fix ListError component
    Import the correct component

--- src/components/Article/CommentContainer.js
+++ src/components/Article/CommentContainer.js
@@ -1,14 +1,16 @@
-import CommentInput from './CommentInput';
-import CommentList from './CommentList';
 import { Link } from 'react-router-dom';
 import React from 'react';
+
+import CommentInput from './CommentInput';
+import CommentList from './CommentList';
+import ListErrors from '../ListErrors';
 
 const CommentContainer = props => {
   if (props.currentUser) {
     return (
       <div className="col-xs-12 col-md-8 offset-md-2">
         <div>
-          <list-errors errors={props.errors}></list-errors>
+          <ListErrors errors={props.errors} />
           <CommentInput slug={props.slug} currentUser={props.currentUser} />
         </div>
 
